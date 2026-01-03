6609535c82594b477f962b7acb4d6fbfb5117724
Tan Kwan Yee <tankwanyee@hotmail.com>
2026-01-03 23:54:56 +0800

    feat: add initial types and context

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -3,3 +3,63 @@
  * [ ] - Hold target's layout states
  * [ ] - Hold states for consumer's control about the components' behaviour
  */
+import { createContext, useMemo, useState } from 'react';
+import { Text } from 'react-native';
+import { useAnimatedRef } from 'react-native-reanimated';
+import type {
+  InstanceOrElement,
+  MeasuredDimensions,
+} from 'react-native-reanimated/lib/typescript/commonTypes';
+import {
+  type ContextMenuContextValue,
+  type ContextMenuExternalProps,
+  OverflowAnchorTreatment,
+} from '../types/ContextMenu.types';
+
+const defaultCallbacks = () => console.warn('Has to be use within ContextMenuContext');
+
+export const ContextMenuContext = createContext<ContextMenuContextValue>({
+  anchorAnimatedRef: null,
+  anchorLayout: null,
+  contextMenuListContainerStyle: {},
+  open: false,
+  overflowAnchorTreatment: OverflowAnchorTreatment.ANIMATE_INTO_VIEW,
+  sections: [],
+  setAnchorLayout: defaultCallbacks,
+  setInternalOpen: defaultCallbacks,
+});
+
+export const ContextMenu = (props: ContextMenuExternalProps) => {
+  const { contextMenuListContainerStyle, open, overflowAnchorTreatment, sections } = props;
+
+  const [internalOpen, setInternalOpen] = useState(false);
+  const [anchorLayout, setAnchorLayout] = useState<MeasuredDimensions | null>(null);
+  const anchorAnimatedRef = useAnimatedRef<InstanceOrElement>();
+
+  const value = useMemo(() => {
+    return {
+      anchorAnimatedRef,
+      anchorLayout,
+      contextMenuListContainerStyle,
+      open: open ?? internalOpen,
+      overflowAnchorTreatment,
+      sections,
+      setAnchorLayout,
+      setInternalOpen,
+    };
+  }, [
+    anchorAnimatedRef,
+    anchorLayout,
+    contextMenuListContainerStyle,
+    internalOpen,
+    open,
+    overflowAnchorTreatment,
+    sections,
+  ]);
+
+  return (
+    <ContextMenuContext value={value}>
+      <Text>ContextMenu</Text>
+    </ContextMenuContext>
+  );
+};
---
+++ src/types/ContextMenu.types.ts
@@ -0,0 +1,42 @@
+import { type ReactNode } from 'react';
+import { type StyleProp, type ViewStyle } from 'react-native';
+import { type AnimatedRef } from 'react-native-reanimated';
+import type {
+  InstanceOrElement,
+  MeasuredDimensions,
+} from 'react-native-reanimated/lib/typescript/commonTypes';
+
+export interface ContextMenuListItem {
+  label: string;
+  leadingIcon?: ReactNode;
+  onPress: () => void;
+  trailingIcon?: ReactNode;
+}
+
+export interface ContextMenuListSection {
+  items: ContextMenuListItem[];
+  sectionTitle?: string;
+}
+
+export enum OverflowAnchorTreatment {
+  ANIMATE_INTO_VIEW = 'animate-into-view',
+  OVERRIDE = 'override',
+  SHRINK_TO_FIT = 'shrink-to-fit',
+}
+
+export interface ContextMenuExternalProps {
+  contextMenuListContainerStyle: StyleProp<ViewStyle>;
+  open?: boolean;
+  overflowAnchorTreatment: OverflowAnchorTreatment;
+  sections: ContextMenuListSection[];
+}
+
+export interface ContextMenuInternalStates {
+  anchorAnimatedRef: AnimatedRef<InstanceOrElement> | null;
+  anchorLayout: MeasuredDimensions | null;
+  contextMenuListContainerStyle: StyleProp<ViewStyle>;
+  setAnchorLayout: (layout: MeasuredDimensions) => void;
+  setInternalOpen: (open: boolean) => void;
+}
+
+export type ContextMenuContextValue = ContextMenuInternalStates & ContextMenuExternalProps;
